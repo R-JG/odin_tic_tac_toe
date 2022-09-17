8fe7cbc1abe19ad25195825acec41a29abcf7be3
Robin <robin.james.guerrier@gmail.com>
2022-09-17 19:02:16 -0400

    Add the gameState module. Make playerMove a method in gameState. Begin the main game loop.

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-
-/**
-   Game board:
- * The game board is an object - module 
- * Game board squares are assigned with coordinate values (123 + abc)
- * 
-   Players:
- * Players are objects - factory
- * Making a move pushes coordinate values to two array properties
- * i.e. an array for x coordinates and an array for y coordinates
- * 
-   Rounds:
- * A game state object with methods that run for each round?
- * The round logic checks the player object for the predefined winning conditions
- * Use conjunctions of array.includes() to check the arrays for the winning conditions
- * 
- */
-
 const winScenarios = {
     horizontalA: ["A1", "A2", "A3"],
     horizontalB: ["B1", "B2", "B3"],
@@ -25,18 +7,16 @@ const winScenarios = {
     verticalThree: ["A3", "B3", "C3"],
     diagonalFirst: ["A1", "B2", "C3"],
     diagonalSecond: ["A3", "B2", "C1"],
-}
-// is there a way to constrain the possible win scenarios based on the coordinates of a given square object?
+};
 
-
-const newPlayer = function() {
+const newPlayer = function(newName) {
+    let name = newName;
     let moves = [];
-    return { moves };
-}
-
+    let wins = 0;
+    return { name, moves, wins };
+};
 
 const gameBoard = (function() {
-
     let gameBoardSquares = [];
     const xCoordinateArray = [1, 2, 3];
     const yCoordinateArray = ["A","B","C"];
@@ -53,39 +33,78 @@ const gameBoard = (function() {
     };
 
     function createDisplay() {
-
         const gameBoardElement = document.querySelector("#game-board");
 
         for (let i = 0; i < gameBoardSquares.length; i++) {
             const boardSquareElement = document.createElement("div");
             boardSquareElement.classList.add("board-square");
-   
+    
             boardSquareElement.addEventListener("click", (e) => {
-                playerMove(playerOne, gameBoardSquares[i].xCoordinate, gameBoardSquares[i].yCoordinate);
+                gameState.playerMove(gameBoardSquares[i].xCoordinate, gameBoardSquares[i].yCoordinate);
             });
-
+    
             gameBoardElement.appendChild(boardSquareElement);
         };
     };
-
     return { createDisplay };
 })();
 
-function playerMove(player, xCoordinate, yCoordinate) {
-    const move = yCoordinate + xCoordinate;
-    player.moves.push(move);
+const gameState = (function() {
+    let playerOne;
+    let playerTwo;
+    let activePlayer;
 
-    // check if the player meets one of the win conditions:
-    for (coordiantes in winScenarios) {
-        if (winScenarios[coordiantes].every((coordinate) => player.moves.includes(coordinate))) {
-            console.log("you win!")
-            break;
+    function initializeGame() {
+        const startButton = document.querySelector("#start-button");
+        startButton.addEventListener("click", (e) => {
+            const playerOneName = prompt("Enter a name for player 1:");
+            const playerTwoName = prompt("Enter a name for player 2:");
+    
+            playerOne = newPlayer(playerOneName);
+            playerTwo = newPlayer(playerTwoName);
+            activePlayer = playerOne;
+    
+            startButton.style.display = "none";
+            gameBoard.createDisplay();
+        });
+    };
+
+    function playerMove(xCoordinate, yCoordinate) {
+        const move = yCoordinate + xCoordinate;
+        activePlayer.moves.push(move);
+    
+        //test
+        console.log(`activePlayer ${activePlayer.name}: ${activePlayer.moves}`);
+        console.log(`playerOne ${playerOne.name}: ${playerOne.moves}`);
+        console.log(`playerTwo ${playerTwo.name}: ${playerTwo.moves}`);
+    
+        // check if the active player meets one of the win conditions:
+        for (scenario in winScenarios) {
+            if (winScenarios[scenario].every((coordinate) => activePlayer.moves.includes(coordinate))) {
+                console.log(`${activePlayer.name} wins!`);
+                break;
+            };
         };
     };
+
+    return { playerOne, playerTwo, activePlayer, initializeGame, playerMove };
+})();
+
+function main() {
+    gameState.initializeGame();
+
+    do {
+        console.log(`${gameState[activePlayer].name}, make a move!`);
+    
+        // alternate player one and two's turns:
+        switch (gameState.activePlayer) {
+            case gameState.playerOne: 
+                gameState.activePlayer = gameState.playerTwo;
+                break;
+            case gameState.playerTwo:
+                gameState.activePlayer = gameState.playerOne;
+        }
+    } while ((gameState[playerOne].wins === 0) && (gameState[playerTwo].wins === 0));
 };
 
-
-
-gameBoard.createDisplay();
-
-let playerOne = newPlayer();
+main();
